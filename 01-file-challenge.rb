3d64c3505286a1323c03b8dca4ea4b61fc78f4ad
Harrison Malone <harrison.malone2@gmail.com>
2019-09-02 10:56:47 +1000

    updated files challenge

--- 01-file-challenge.rb
+++ 01-file-challenge.rb
@@ -2,19 +2,17 @@
 
 # 1. Create a constant for the todo list file name
   
-# 2. Write a method to display the to do list, pass the file name as a parameter
+# 2. Define an empty array named todos, write a method to read the csv items (rows) into the array and then return the array with the populated todos
 
-# 3. Ask the user "what do they want to add to the to do list"
-
-# 4. Read each item in the to do list into an array
-
-# 5. Display each item in the array in a numbered list 
+# 3. Write a method that neatly displays each item in the array in a numbered list 
 # eg: 1. Put out garbage
 
-# 6. Create a menu using a while loop, if the user types "1" then the todos are displayed, if the user types "2" then the while loop exits and the program terminates
+# 4. Write a method that asks the user "what do you want to add to your to do list", and then adds this text to the todos array before writing this updated information back to the csv
 
-# 7. Add the method and options in the menu for adding a todo
+# 5. Create a menu using a while loop, if the user types "1" then the todos are displayed, if the user types "2" then the while loop exits and the program terminates
 
-# 8. Add the method and options in the menu for deleting a todo
+# 6. Add the method and options in the menu for adding a todo
 
-# 9. Add the method and options in the menu for updating a todo
+# 7. Add the method and options in the menu for deleting a todo
+
+# 8. Add the method and options in the menu for updating a todo
